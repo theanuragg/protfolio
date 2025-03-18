e681361c4b68d090d953fddd7161fc2e176cc2c9
Anurag Singh <119996443+theanuragg@users.noreply.github.com>
2025-03-18 15:58:09 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 
 import Header from '../components/Header'
-import ProjectsGrid from '../components/ProjectsGrid'
+
 import Achievements from '../components/Achievements'
 
 export default function Home() {
@@ -8,7 +8,7 @@ export default function Home() {
     <main className="min-h-screen bg-[#0a192f]">
       <div className="max-w-6xl mx-auto px-4">
         <Header />
-        <ProjectsGrid />
+       
         <Achievements />
       </div>
     </main>
